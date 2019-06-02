1093363659b3fab3b449bbb01fa03c1f4fce2204
kevin578 <kevinbriggs1@gmail.com>
2019-06-02 17:37:46 -0400

    installed enzyme

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from '../App';
+import CommentBox from '../CommentBox';
+import CommentList from '../CommentList'
+
+const wrapped = shallow(<App />);
+
+it('shows a comment box', ()=> {
+    expect(wrapped.find(CommentBox).length).toEqual(1);
+})
+
+it('shows a comment list', ()=> {
+    expect(wrapped.find(CommentBox).length).toEqual(1)
+})
