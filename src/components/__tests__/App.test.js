3c1de57c9cfa92b4614bc1510677d5198fc8dc61
kevin578 <kevinbriggs1@gmail.com>
2019-06-05 21:01:15 -0400

    retreieving prop values

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
-import CommentList from '../CommentList'
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList'
 
-const wrapped = shallow(<App />);
+
+let wrapped;
+
+beforeEach(()=> {
+    wrapped = shallow(<App />);
+})
+
 
 it('shows a comment box', ()=> {
     expect(wrapped.find(CommentBox).length).toEqual(1);
